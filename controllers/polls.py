299a22f2735cb7076f06ab525cfa6a17d6a550ba
dmaindonald1 <ummaindo@cc.umanitoba.ca>
2013-02-13 16:42:22 -0600

    polls.py
    Update validators and remove password field

--- controllers/polls.py
+++ controllers/polls.py
@@ -5,16 +5,15 @@ def new():
     import gluon.contrib.simplejson as sj
 
     form = FORM(TABLE(
-        TR('Title:', INPUT(_type='text', _name='title', requires=IS_LENGTH(100, 10))),
-        TR('Description:', TEXTAREA(_type='text', _name='description', requires=IS_EMPTY_OR(IS_LENGTH(255, 10)))),
-		TR('Password:', INPUT(_type='text', _name='password', requires=IS_EMPTY_OR(IS_LENGTH(12, 6)))),
+        TR('Title:', INPUT(_type='text', _name='title', requires=IS_LENGTH(100, 1))),
+        TR('Description:', TEXTAREA(_type='text', _name='description', requires=IS_EMPTY_OR(IS_LENGTH(250, 1)))),
 		TR('Group:', SELECT('All','Studets','Teachers', _name="group",requires=IS_IN_SET(['All','Studets','Teachers']))),
-		TR('Poll Question:', TEXTAREA(_name='question', requires=IS_LENGTH(255, 10))),
-		TR('Answer #1:', INPUT(_type='text', _name='answer1', requires=IS_LENGTH(100, 10))),
-		TR('Answer #2:', INPUT(_type='text', _name='answer2', requires=IS_LENGTH(100, 10))),
-		TR('Answer #3:', INPUT(_type='text', _name='answer3', requires=IS_EMPTY_OR(IS_LENGTH(100, 10)))),
-		TR('Answer #4:', INPUT(_type='text', _name='answer4', requires=IS_EMPTY_OR(IS_LENGTH(100, 10)))),
-		TR('Answer #5:', INPUT(_type='text', _name='answer5', requires=IS_EMPTY_OR(IS_LENGTH(100, 10)))),
+		TR('Poll Question:', TEXTAREA(_name='question', requires=IS_LENGTH(250, 1))),
+		TR('Answer #1:', INPUT(_type='text', _name='answer1', requires=IS_LENGTH(100, 1))),
+		TR('Answer #2:', INPUT(_type='text', _name='answer2', requires=IS_LENGTH(100, 1))),
+		TR('Answer #3:', INPUT(_type='text', _name='answer3', requires=IS_EMPTY_OR(IS_LENGTH(100, 1)))),
+		TR('Answer #4:', INPUT(_type='text', _name='answer4', requires=IS_EMPTY_OR(IS_LENGTH(100, 1)))),
+		TR('Answer #5:', INPUT(_type='text', _name='answer5', requires=IS_EMPTY_OR(IS_LENGTH(100, 1)))),
         TR('', INPUT(_type='submit', _value='SUBMIT')),
     ))
     if form.process().accepted:
