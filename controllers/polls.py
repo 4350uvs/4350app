60b0ec538505d72a811f18c9dd6b748f076f7800
Umbruner <umbruner@cc.umanitoba.ca>
2013-03-01 21:33:38 -0600

    Updates to views, CSS, poll.py controller

--- controllers/polls.py
+++ controllers/polls.py
@@ -1,20 +1,20 @@
 def index():
-    return dict(jsonPolls = api.getJsonDict('/polls'))
+	return dict(jsonPolls = api.getJsonDict('polls'))
 
 def new():
-    import gluon.contrib.simplejson as sj
+	import gluon.contrib.simplejson as sj
 
-    form = FORM(TABLE(
-        TR('Title:', INPUT(_type='text', _name='title', 
+	form = FORM(TABLE(
+		TR('Title:', INPUT(_type='text', _name='title', 
 			requires=	[IS_NOT_EMPTY(error_message='Enter a title for your poll'),
 						IS_LENGTH(maxsize=100, error_message='Please keep the title under 100 characters')])),
 						
-        TR('Description:', TEXTAREA(_type='text', _name='description', 
+		TR('Description:', TEXTAREA(_type='text', _name='description', 
 			requires=	IS_EMPTY_OR(IS_LENGTH(maxsize=250, error_message='Please keep the description under 250 characters')))),
 			
 		TR('Group:', SELECT('All','Studets','Teachers', _name="group",
 			requires=	IS_IN_SET(['All','Studets','Teachers']))),
-			
+
 		TR('Poll Question:', TEXTAREA(_name='question', 
 			requires=	[IS_NOT_EMPTY(error_message='Enter a question for your poll'),
 						IS_LENGTH(maxsize=250, error_message='Please keep the question under 100 characters')])),
@@ -26,7 +26,7 @@ def new():
 		TR('Answer #2:', INPUT(_type='text', _name='answer2', 
 			requires=	[IS_NOT_EMPTY(error_message='Enter another answer for your poll'),
 						IS_LENGTH(maxsize=100, error_message='Please keep the answer under 100 characters')])),
-						
+
 		TR('Answer #3:', INPUT(_type='text', _name='answer3', 
 			requires=	IS_EMPTY_OR(IS_LENGTH(maxsize=100, error_message='Please keep the answer under 100 characters')))),
 			
@@ -35,13 +35,13 @@ def new():
 			
 		TR('Answer #5:', INPUT(_type='text', _name='answer5', 
 			requires=	IS_EMPTY_OR(IS_LENGTH(maxsize=100, error_message='Please keep the answer under 100 characters')))),
-			
-        TR('', INPUT(_type='submit', _value='SUBMIT')),
-    ))
-    if form.process().accepted:
-        response.flash = 'Your poll has been accepted'
-    elif form.errors:
-        response.flash = 'Please fill out the highlighted fields'
-    else:
-        response.flash = 'Please enter your poll information'
-    return dict(form=form, vars=sj.dumps(form.vars))
+
+		TR('', INPUT(_type='submit', _value='SUBMIT')),
+	))
+	if form.process().accepted:
+		response.flash = 'Your poll has been accepted'
+	elif form.errors:
+		response.flash = 'Please fill out the highlighted fields'
+	else:
+		response.flash = 'Please enter your poll information'
+	return dict(form=form, vars=sj.dumps(form.vars))
